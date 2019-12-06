da6b5470518da49985506d3886f7ba0902ba2a16
André Pontes <contato@andrehost.com.br>
2019-12-06 03:07:31 -0400

    Configuração de Setup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 6.0
+                    Laravel 6.0.0
                 </div>
 
                 <div class="links">
